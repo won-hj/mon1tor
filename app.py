bc87297f417b671261a8fae9565fc209213bf37f
1hjwon <whjbssc@gmail.com>
2023-04-15 02:34:08 +0900

    기본 라우팅 테스트 및 생성

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'hello world!!!'
+    return 'hello world'
+
+@app.route('/demo') #<//!-- methods=['POST'] --> 
+def demo():
+    return render_template('demo.html')
 
 if __name__ == '__main__':
     app.run()
