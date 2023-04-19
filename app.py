7ea86d849452bb31f103566a3cec478e5fc72503
1hjwon <whjbssc@gmail.com>
2023-04-19 14:37:43 +0900

    주석 추가

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
+    """
+    @app.route에 설정된 '/' 로 접속하면 hello world 출력
+    localhost:5000/ 로 접속
+    """
     return 'hello world'
 
-@app.route('/demo') #<//!-- methods=['POST'] --> 
+
+@app.route('/demo')
 def demo():
+    """
+    @app.route에 설정된 '/demo'로 접속하면 demo.html 화면 출력
+    localhost:5000/demo.html 로 접속
+    """
     return render_template('demo.html')
 
 if __name__ == '__main__':
