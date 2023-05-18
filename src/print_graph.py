0d8df58b7c1ff3c2e8de918634e7e02465f89e49
1hjwon <whjbssc@gmail.com>
2023-05-18 21:42:27 +0900

    print_graph 수정

--- src/print_graph.py
+++ src/print_graph.py
@@ -5,27 +5,33 @@ from bokeh.palettes import Spectral4
 import csv
 from bokeh.models import BasicTickFormatter, NumeralTickFormatter
 from bokeh.layouts import gridplot
+import logging
 
 '''
-    path 경로의 파일들의 개수를 리스트로 반환
+    path 경로의 파일들을 리스트나 개수로 반환
 '''
-def get_files(path):
+def get_files(path, option=0):
     import os
 
     dir_list = os.listdir(path)
-    return (dir_list[:-1])
+    if option == 0:
+        return (dir_list[:-1])
+    else:
+        return len(dir_list[:-1])
 
+def get_location():
+    return '../tool/'
 # 1. 지금 디버그툴 깔려있지 않아서 적절한 것을 찾아야 한다.
 
 # 딕셔너리에 {년도:데이터리스트} 로 저장
 def get_csv():
-    location = '../tool/'
-    file = get_files(location)
+    file = get_files(get_location(), 0)
+    
+    print(file, 'file') #비어있네
     set_figure = {} 
     
-
-    for i in range(len(file)):
-        with open (location+file[i], encoding='UTF-8') as f:
+    for i in range(len(file)): #len(file)
+        with open ('../tool/'+ str(2013+i) +'data.csv', encoding='UTF-8') as f:
             reader = csv.reader(f)
             birth_death_data = []
             age_data = []
@@ -37,9 +43,9 @@ def get_csv():
                 elif row[0] == '생산가능인구(15-64)' or row[0] == '고령인구(65-)':
                     age_data.append(row)
                 set_figure[file[i]] = [birth_death_data, age_data]
+    
     return set_figure
     
 if __name__ == "__main__":
-    get_csv()
+    #get_csv()
     print(get_csv())
-    
