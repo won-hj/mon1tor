aaaa9ede6951ab71c2d214e03d2afd2cccd0441f
Dohyun Kim <sks020k@naver.com>
2023-05-21 00:00:45 +0900

    UI개선&5개년추출

--- prediction graph/2023-2027 graph.py
+++ prediction graph/2023-2027 graph.py
@@ -3,54 +3,78 @@ from prophet import Prophet
 from bokeh.plotting import figure, show
 from bokeh.models import ColumnDataSource
 from bokeh.models.formatters import NumeralTickFormatter, DatetimeTickFormatter
-from bokeh.models import ColumnDataSource, HoverTool, Title
+from bokeh.models import HoverTool, Title
+
+
+class ForecastPlotter:
+    def __init__(self, data, target_columns, title):
+        self.data = data
+        self.target_columns = target_columns
+        self.title = title
+
+    def preprocess_data(self):#bokeh 사용을 위해 datetime형식으로 변환
+        self.data['year'] = pd.to_datetime(self.data['year'], format='%Y')
+
+    def fit_models(self): #prophet예측
+        m_births = Prophet(changepoint_prior_scale=0.001, seasonality_mode='multiplicative', yearly_seasonality=5)
+        m_deaths = Prophet(changepoint_prior_scale=0.001, seasonality_mode='multiplicative', yearly_seasonality=5)
+
+        births_data = self.data[['year', self.target_columns[0]]].rename(columns={'year': 'ds', self.target_columns[0]: 'y'})
+        deaths_data = self.data[['year', self.target_columns[1]]].rename(columns={'year': 'ds', self.target_columns[1]: 'y'})
+
+        m_births.fit(births_data)
+        m_deaths.fit(deaths_data)
+
+        future_start_date = pd.to_datetime('2022')
+        future_end_date = pd.to_datetime('2027')
+
+        future_dates = pd.date_range(start=future_start_date, end=future_end_date, freq='Y')
+
+        self.forecast_births = m_births.predict(pd.DataFrame({'ds': future_dates}))
+        self.forecast_deaths = m_deaths.predict(pd.DataFrame({'ds': future_dates}))
+
+    def plot(self):#bokeh시각화
+        self.preprocess_data()
+        self.fit_models()
+
+        p = figure(title=Title(text=self.title, align="center", text_font_size="22px", text_font="Consolas", text_font_style="bold"),
+                    x_axis_type='datetime', width=1200, height=500)
+
+        
+        p.xaxis.formatter = DatetimeTickFormatter(years='%Y')
+        
+        births_hover_tool = HoverTool(tooltips=[("출생아 수", "@y{0,0}")], formatters={"@x": "datetime"})
+        deaths_hover_tool = HoverTool(tooltips=[("사망자 수", "@y{0,0}")], formatters={"@x": "datetime"})
+
+        p.add_tools(births_hover_tool)
+        p.add_tools(deaths_hover_tool)
+
+        source_births = ColumnDataSource(data=dict(
+            x=self.forecast_births['ds'],
+            y=self.forecast_births['yhat'],
+            y_upper=self.forecast_births['yhat_upper'],
+            y_lower=self.forecast_births['yhat_lower']
+        ))
+
+        source_deaths = ColumnDataSource(data=dict(
+            x=self.forecast_deaths['ds'],
+            y=self.forecast_deaths['yhat'],
+            y_upper=self.forecast_deaths['yhat_upper'],
+            y_lower=self.forecast_deaths['yhat_lower']
+        ))
+
+        p.line(x='x', y='y', source=source_births, color='red', legend_label='출생아 수')
+        p.varea(x='x', y1='y_lower', y2='y_upper', source=source_births, color='red', alpha=0.1)
+        p.line(x='x', y='y', source=source_deaths, color='blue', legend_label='사망자 수')
+        p.varea(x='x', y1='y_lower', y2='y_upper', source=source_deaths, color='blue', alpha=0.1)
+
+        p.yaxis.formatter = NumeralTickFormatter(format="0,0")
+        p.legend.location = "top_left"
+        p.legend.orientation = "horizontal"
+
+        show(p)
 
 data = pd.read_csv('./first_data.csv')
 
-data['year'] = pd.to_datetime(data['year'], format='%Y')
-births_data = data[['year', 'births']].rename(columns={'year': 'ds', 'births': 'y'})
-deaths_data = data[['year', 'deaths']].rename(columns={'year': 'ds', 'deaths': 'y'})
-
-m_births = Prophet(changepoint_prior_scale=0.001, seasonality_mode='multiplicative', yearly_seasonality=5)
-m_deaths = Prophet(changepoint_prior_scale=0.001, seasonality_mode='multiplicative', yearly_seasonality=5)
-
-m_births.fit(births_data)
-m_deaths.fit(deaths_data)
-
-future_births = m_births.make_future_dataframe(periods=5, freq='Y')
-future_deaths = m_deaths.make_future_dataframe(periods=5, freq='Y')
-
-forecast_births = m_births.predict(future_births)
-forecast_deaths = m_deaths.predict(future_deaths)
-
-p = figure(title='Prediction Graph', x_axis_label='Year', y_axis_label='Births/Deaths', x_axis_type='datetime', width=1200, height=500)
-
-p.xaxis[0].ticker.desired_num_ticks = len(forecast_births) + 1
-p.xaxis[0].ticker.num_minor_ticks = 0
-p.xaxis.formatter = DatetimeTickFormatter(years='%Y')
-
-source_births = ColumnDataSource(data=dict(
-    x=forecast_births['ds'],
-    y=forecast_births['yhat'],
-    y_upper=forecast_births['yhat_upper'],
-    y_lower=forecast_births['yhat_lower']
-))
-
-source_deaths = ColumnDataSource(data=dict(
-    x=forecast_deaths['ds'],
-    y=forecast_deaths['yhat'],
-    y_upper=forecast_deaths['yhat_upper'],
-    y_lower=forecast_deaths['yhat_lower']
-))
-
-p.line(x='x', y='y', source=source_births, color='red', legend_label='Births')
-p.varea(x='x', y1='y_lower', y2='y_upper', source=source_births, color='red', alpha=0.1)
-p.line(x='x', y='y', source=source_deaths, color='blue', legend_label='Deaths')
-p.varea(x='x', y1='y_lower', y2='y_upper', source=source_deaths, color='blue', alpha=0.1)
-
-p.yaxis.formatter = NumeralTickFormatter(format="0,0")
-
-hover_tool = HoverTool(tooltips=[("Year", "@x{%Y}"), ("Number of births", "@y{0,0}")], formatters={"@x": "datetime"})
-p.add_tools(hover_tool)
-
-show(p)
+forecast_plotter = ForecastPlotter(data, ['births', 'deaths'], '2023-2027')
+forecast_plotter.plot()
