5a7168b5c24bdfbe65344d3ee9864ec87b91d9b0
Dohyun Kim <sks020k@naver.com>
2023-05-21 20:57:56 +0900

    csv 구성 위한 가공

--- Data pipeline/2023-2027 csv 추출.py
+++ Data pipeline/2023-2027 csv 추출.py
@@ -4,10 +4,19 @@ from prophet import Prophet
 def load_original_data(file_path): #return 기존 csv 경로
     return pd.read_csv(file_path)
 
-def fit_prophet_model(data):    #5개년 예측 return 예측model
+def fit_prophet_model(data):    #return prophet 예측 model
     model = Prophet(changepoint_prior_scale=0.001, seasonality_mode='multiplicative', yearly_seasonality=5)
     model.fit(data)
     return model
+
+def predict_future_data(model, future_dates): #return 5개년에 대한 예측 value
+    forecast = model.predict(pd.DataFrame({'ds': future_dates}))
+    return forecast['yhat'].astype(int)
+
+def create_yearly_data(start_year, end_year): #return df with columns
+    years = range(start_year, end_year + 1)
+    data = pd.DataFrame({'Year': years, 'Birth': [0] * len(years), 'Death': [0] * len(years)})
+    return data
 
 start_year = 2023
 end_year = 2027
@@ -18,3 +27,8 @@ m_births = fit_prophet_model(predicted_data[['year', 'births']].rename(columns={
 m_deaths = fit_prophet_model(predicted_data[['year', 'deaths']].rename(columns={'year': 'ds', 'deaths': 'y'}))
 
 future_dates = pd.date_range(start=str(start_year - 1), end=str(end_year + 1), freq='Y')
+
+forecast_births = predict_future_data(m_births, future_dates)
+forecast_deaths = predict_future_data(m_deaths, future_dates)
+
+data = create_yearly_data(start_year, end_year)
