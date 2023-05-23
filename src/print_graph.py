7af22de5979caefefbe011fc04951b61bffee559
1hjwon <whjbssc@gmail.com>
2023-05-23 22:14:22 +0900

    print_graph 수정

--- src/print_graph.py
+++ src/print_graph.py
@@ -6,6 +6,7 @@ import csv
 from bokeh.models import BasicTickFormatter, NumeralTickFormatter
 from bokeh.layouts import gridplot
 import logging
+import os
 
 '''
     path 경로의 파일들을 리스트나 개수로 반환
@@ -13,27 +14,26 @@ import logging
            1 : 파일 개수 반환
 '''
 def get_files(path, option=0):
-    import os
-
-    dir_list = os.listdir(path)
+    dir_list = path 
     if option == 0:
         return (dir_list[:-1])
     else:
         return len(dir_list[:-1])
 
 def get_location():
-    return '../tool/'
-# 1. 지금 디버그툴 깔려있지 않아서 적절한 것을 찾아야 한다.
+    cwd = os.getcwd()
+    path = os.path.join(cwd, '/../tool ')
+    return path
 
 # 딕셔너리에 {년도:데이터리스트} 로 저장
 def get_csv():
     file = get_files(get_location(), 0)
-    
-    print(file, 'file') #비어있네
+
+    print(file, 'file') #출력용???
     set_figure = {} 
     
-    for i in range(len(file)): #len(file)
-        with open ('../tool/'+ str(2013+i) +'data.csv', encoding='UTF-8') as f:
+    for i in range(len(file)): 
+        with open ('./tool/'+ str(2013+i) +'data.csv', encoding='UTF-8') as f:
             reader = csv.reader(f)
             birth_death_data = []
             age_data = []
@@ -49,5 +49,4 @@ def get_csv():
     return set_figure
     
 if __name__ == "__main__":
-    #get_csv()
-    print(get_csv())
+    get_csv()
