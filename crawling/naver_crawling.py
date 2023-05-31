e504348121e7393c7bb0bd0d8eaebf50af23c866
Dohyun Kim <sks020k@naver.com>
2023-05-31 17:32:14 +0900

    API요청 시 None반환 해결

--- crawling/naver_crawling.py
+++ crawling/naver_crawling.py
@@ -4,7 +4,7 @@ import urllib.request
 import requests
 #api 사용해서 불러오기
 client_id = "lg542JOAVw0xKmjJ1Nlf"
-client_secret = "BIjuZXEvKb"
+client_secret = "mArt8cw08s"
 url = "https://openapi.naver.com/v1/search/news.json" # JSON 결과
 
 #display_num : 한 번에 표시할 검색 결과 개수 (default : 10 max : 100)
@@ -14,8 +14,15 @@ def keyword(key, display_num): #return 크롤링결과 json형식 / key : 검색
     params = {'query': key,
               'display':display_num,
               'sort':'sim'} #sim:정확도 순으로 정렬(default)
-    r = requests.get(url,params= params, headers= headers).json()['items']
-    return r
+    response = requests.get(url,params=params,headers=headers)
+    response_json = response.json()
+    if response.status_code != 200: 
+        print(f"Error: {response.status_code}")
+        print(response_json) 
+        return 
 
+    r = response_json.get('items',[])
+
+    return r 
 
 
