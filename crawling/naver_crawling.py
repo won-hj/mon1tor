8e4e46f85ba0ab3fa200d1951822051713b2471e
Dohyun Kim <sks020k@naver.com>
2023-05-31 16:37:17 +0900

    크롤링 method 구성

--- crawling/naver_crawling.py
+++ crawling/naver_crawling.py
@@ -1,9 +1,21 @@
-# 네이버 검색 API 예제 - 블로그 검색
 import os
 import sys
 import urllib.request
+import requests
+#api 사용해서 불러오기
 client_id = "lg542JOAVw0xKmjJ1Nlf"
 client_secret = "BIjuZXEvKb"
-encText = urllib.parse.quote("저출산 산업")
-url = "https://openapi.naver.com/v1/search/blog?query=" + encText # JSON 결과
-# url = "https://openapi.naver.com/v1/search/blog.xml?query=" + encText # XML 결과
+url = "https://openapi.naver.com/v1/search/news.json" # JSON 결과
+
+#display_num : 한 번에 표시할 검색 결과 개수 (default : 10 max : 100)
+def keyword(key, display_num): #return 크롤링결과 json형식 / key : 검색 단어, display_num: 가져올 페이지 개수
+    headers = {'X-Naver-Client-Id' : client_id,
+               'X-Naver-Client-Secret' : client_secret}
+    params = {'query': key,
+              'display':display_num,
+              'sort':'sim'} #sim:정확도 순으로 정렬(default)
+    r = requests.get(url,params= params, headers= headers).json()['items']
+    return r
+
+
+
