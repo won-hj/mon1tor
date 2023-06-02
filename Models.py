a1c5af0f1b0ffe3dd94aa3bfc92fe3fdcf9e1f2f
Dohyun Kim <sks020k@naver.com>
2023-06-02 19:48:12 +0900

    password 해싱 method

--- Models.py
+++ Models.py
@@ -12,3 +12,10 @@ class User(db.Model): #데이터 모델(SQL table)을 나타내는 객체
 
     def __init__(self, email, password): #User클래스 생성자
         self.email = email
+        self.set_password(password)
+    
+    def set_password(self, password): # 비밀번호를 해싱해서 저장
+        self.password = generate_password_hash(password)
+ 
+    def check_password(self, password): # return compare(해싱된 비밀번호,입력된 비밀번호)
+        return check_password_hash(self.password, password)
