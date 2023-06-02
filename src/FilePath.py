c8b0cfc6bc4d09d0da56ce7329c2befe8c99c982
1hjwon <whjbssc@gmail.com>
2023-06-03 03:21:47 +0900

    config/filepath 한번에 관리하려고 했는데 어떻게해야 적용이 잘 되는지는 고민중...

--- src/FilePath.py
+++ src/FilePath.py
@@ -1,23 +1,43 @@
-from configparser import ConfigParser
+#from configparser import ConfigParser
+import sys
+import configparser as c
+import os
 
 class path:
-    def __init__(self) -> None:
+    def __init__(self, cwd) -> None:
         pass
-        self.parser = ConfigParser()
-        self.config = '../config/config.ini'
-        self.parser.read(self.config, encoding='utf-8')
+        self.cwd = cwd
+        try:
+            self.parser = c. ConfigParser()
+            self.config = os.getcwd() + cwd #'./../config/config.ini'
+            self.parser.read(self.config, encoding='utf-8')
+
+        except c. ConfigParser.NoSectionError:
+            sys.stderr.write("Could not open %s\n" % self.settingsFilename)
+            sys.stderr.write("Exiting...\n")
+            sys.exit(1)
 
     #folder = {'birthover' : 1, 'birthunder' : 2, 'workover' : 3, 'workunder' : 4}
     folder = { 1 : 'birthover', 2 : 'birthunder', 3 : 'workover', 4 : 'workunder'}
 
     def get_parser(self):
         return self.parser
+    
+    def get_path(self,num):
+        path = self.parser.get('config', self.folder[num])
+        return path
+    
+    def get_configpath(self):
+        return self.config
 
-    def get_path(self, num):
-        return self.parser.get('config', self.folder[num])
-
+    def get_now(self):
+        return os.getcwd() +':'+ self.cwd
+    
 
 if __name__ == "__main__":
-    #print(path.get_path())
-    p = path()
-    print(p.get_path(1))
+    p = path('./../config/config.ini')
+    for i in range(1, 5):
+        print(p.get_path(i))
+
+    import os
+    print(os.getcwd()+'\n'+p.get_now())
