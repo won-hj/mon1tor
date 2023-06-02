f4b1505da34e73f2e776f76d6e0b3a2b666b1cc2
1hjwon <whjbssc@gmail.com>
2023-06-02 22:19:00 +0900

    filepath 구조 변경

--- src/FilePath.py
+++ src/FilePath.py
@@ -1,17 +1,23 @@
 from configparser import ConfigParser
 
-parser = ConfigParser()
-config = '../config/config.ini'
-parser.read(config, encoding='utf-8')
+class path:
+    def __init__(self) -> None:
+        pass
+        self.parser = ConfigParser()
+        self.config = '../config/config.ini'
+        self.parser.read(self.config, encoding='utf-8')
 
-#folder = {'birthover' : 1, 'birthunder' : 2, 'workover' : 3, 'workunder' : 4}
-folder = { 1 : 'birthover', 2 : 'birthunder', 3 : 'workover', 4 : 'workunder'}
+    #folder = {'birthover' : 1, 'birthunder' : 2, 'workover' : 3, 'workunder' : 4}
+    folder = { 1 : 'birthover', 2 : 'birthunder', 3 : 'workover', 4 : 'workunder'}
 
-def get_parser():
-    return parser
+    def get_parser(self):
+        return self.parser
 
-def get_path(num):
-    return parser.get('config', folder[num])
+    def get_path(self, num):
+        return self.parser.get('config', self.folder[num])
+
 
 if __name__ == "__main__":
-    print(get_path(1))
+    #print(path.get_path())
+    p = path()
+    print(p.get_path(1))
