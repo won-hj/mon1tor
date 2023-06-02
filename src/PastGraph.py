7aab11fb44713277f5f725e86c75f06b1e33bd94
1hjwon <whjbssc@gmail.com>
2023-06-03 03:20:39 +0900

    출/사 표현 어떻게 처리해야할지 고민중

--- src/PastGraph.py
+++ src/PastGraph.py
@@ -1,3 +1,19 @@
 #여기서 filepath 가지고 왔다갔다..
+from typing import Any
 import FilePath as fp
+from past_graph import *
 
+#fp('config/birth_death/over2022')
+class pastgraph:
+    def __init__(self, config) -> None:
+        pass
+        self.config = config
+        
+    def print(self):
+        pass
+        
+    
+
+if __name__ == "__main__":
+    pass
+        
