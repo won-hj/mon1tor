2a37acc1d834fbb3e37edf85f467d34aa8c35f2c
Dohyun Kim <sks020k@naver.com>
2023-06-07 01:48:50 +0900

    fix:directory path&fix:ready for rendering

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'hello world!!!'
+def index():
+    return render_template('index.html')
 
-if __name__ == '__main__':
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/past_graph1')
+def past_graph1():
+    return render_template('past_graph1.html')
+
+@app.route('/past_graph2')
+def past_graph2():
+    return render_template('past_graph2.html')
+
+@app.route('/prediction')
+def prediction():
+    return prediction('prediction.html')
+
+if __name__ == "__main__":
     app.run()
