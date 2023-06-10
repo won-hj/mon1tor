75dff49a177d54bb2f8dd3ee907756ec321f6911
KimEulJoong <kimeuljoong97@gmail.com>
2023-06-10 13:37:19 +0900

    requirements 텍스트에는 logging 기능이 추가되었습니다. 네이버 크롤링 파일은 <li>값 추가를 통해 리스트 형태로 일괄 정리하였습니다. 확인은 prediction 페이지에서 가능합니다.

--- crawling/naver_crawling.py
+++ crawling/naver_crawling.py
@@ -100,11 +100,7 @@ def predict(age=None):
         for i in range(len(news['Title'])):
             news['Title'].iloc[i] = extract_word(news['Title'].iloc[i])
 
-<<<<<<< HEAD
-        news['Link'] = '<a href="'+ news['Link'] + '">' + news['Title'] + '</a>'
-=======
         news['Link'] = '<li><a href="'+ news['Link'] + '">' + news['Title'] + '</a></li>' # 리스트 형태를 만들기 위해 <li>추기장
->>>>>>> 5ebea2217fd3b0ea07b6b24c8e5c881237125803
         news_all = pd.concat([news_all, news[['Link']]])
 
     links_html = "<br>".join(news_all['Link'])
